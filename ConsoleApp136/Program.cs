8d14bc6647e7f7a441aec54dfec311eddeb07f5f
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-09 18:12:04 +0330

    Add files via upload

---
+++ ConsoleApp136/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp136
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("please enter an integer numerator:");
+            var numerator = int.Parse(Console.ReadLine());
+
+            Console.Write("please enter an integer denominator:");
+            var denominator = int.Parse(Console.ReadLine());
+
+
+            var result = numerator / denominator;
+
+            Console.WriteLine($"\nResult:{numerator}/{denominator}={result}");
+                
+            Console.ReadLine();
+        }
+    }
+}
+
